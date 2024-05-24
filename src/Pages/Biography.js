faef4eebf284433d0c52794ec4aea880d6ee920e
Automoroboto <roberto@studiodespirito.co.uk>
2024-05-24 17:31:03 +0100

    restyle bio

--- src/Pages/Biography.js
+++ src/Pages/Biography.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Carousel from '../Components/Carousel';
+import Mike from '../Images/MikeBlueSuit.png';
 import './Biography.css';
 import AnimatedPage from "../Components/transition";
 
@@ -10,11 +10,19 @@ export default function Biography() {
         <AnimatedPage>
         <div>
        <h2>Biography</h2>
-            <Carousel/>
-            <h3>Studies</h3>
+       <div className='bioImageContainer'>
+       <img src={Mike} alt="Mike" className="bioImage"/>
+       </div>
+           
             <div className="bio">
-<p className='bioText'> "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
-<p className='bioText'>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+<p className='bioText'> Michael is a conductor based in Lyon, France with notable experience working with Opera de Lyon, Longborough Festival Opera, Welsh National Youth Opera, Lambeth Orchestra, and Orchestra Vitae. In training, Michael has also conducted Orchestre National de Lyon, Argovia Philharmonic, Plovdiv Philharmonic and the Orchestre de Chambre de Paris.
+<br></br>
+<br></br>
+Since 2022, Michael is one of the in-house assistant conductors at Opera de Lyon, working with conductors such as Daniele Rustioni, Wayne Marshall, Elena Schwartz and Franck Ollu. Michael also holds positions with London orchestras: Lambeth Orchestra and Orchestra of the Reform. His symphonic work has led to him working with renowned artists such as Jennifer Pike, Nadine Benjamin, Wayne Marshall, Leslie Howard, Michael Foyle, and Benjamin Baker.</p>
+<p className='bioText'>Michael has an MA Performance in Orchestral Conducting from the Royal Welsh College of Music and Drama, where he trained under David Jones and assisted Carlo Rizzi on a production of Verdi's Falstaff. Michael also worked as an assistant conductor for Welsh National Youth Opera during his time in Cardiff, working with Alice Farnham on an award winning production of Sir Peter Maxwell Davis' Kommilitonen!, and Stephen McNeff on a studio recording of his own Welsh language opera Hedd Wyn. 
+<br></br>
+<br></br>
+Michael graduated with a BMus (Hons) from Royal Holloway, University of London, where he discovered conducting with teacher Lev Parikian, and developed his interest in chamber music performance under the guidance of cellist Robert Max and conductor Neil Ferris.</p>
             </div>
         </div>
         </AnimatedPage>
