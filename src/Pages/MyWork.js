d2dbde8b998ae9a443eee71278bdfe8e2dd5fee6
Michael Cobb <michaelcobb@Michaels-MBP.lan>
2024-06-18 07:23:27 +0200

    tidy up of My Work

--- src/Pages/MyWork.js
+++ src/Pages/MyWork.js
@@ -30,61 +30,59 @@ export default function MyWork() {
                    A captivating performance of arse.`,
         },
         {
-          src: Katya,
-          alt: 'Katya',
-          text: `<b>Katia Kabanova</b> February 2023 
-                 <br />Role: Guest Conductor<br /> 
-                 Venue: Royal Opera House
-                 A captivating performance of classic opera.`,
-      },
+            src: Katya,
+            alt: 'Katya',
+            text: `<b>Katia Kabanova</b> February 2023 
+                   <br />Role: Guest Conductor<br /> 
+                   Venue: Opera de Lyon<br />`,
+        },
       
-    {
-        src: laFemme,
-        alt: 'laFemme',
-        text: `<b>La Femme</b> January 2023 
-               <br />Role: Conductor<br /> 
-               Venue: Paris Opera
-               A compelling modern opera.`,
-    },
-    {
-        src: mikesuite,
-        alt: 'mike in a nice blue suit',
-        text: `<b>Katia Kabanova</b> February 2023 
-               <br />Role: Guest Conductor<br /> 
-               Venue: Royal Opera House
-               A captivating performance of classic opera. bum bum`,
-    },
-    
-  {
-    src: Katya,
-    alt: 'Katya',
-    text: `<b>Katia Kabanova, Janacek</b> May 2023 
-           <br />Role: Assistant Conductor to Elena Schwartz<br /> 
-           Venue: Opera de Lyon`,
-},
-{
-    src: conductingOpera,
-    alt: 'Lambeth Orchestra',
-    text: `<b>Lambeth Orchestra 50th Anniversary Concert</b> February 2023 
-           <br />Role: Principal Guest Conductor<br /> 
-           Venue: Queen Elizabeth Hall<br />
-           A celebratory programme including Strauss' Four Last Songs<br />
-           Soloist: Nadine Benjamin`,
-},
-{
-    src: Candide,
-    alt: 'Bernstein Candide',
-    text: `<b>Candide, Bernstein</b> December 2022 
-           <br />Role: Assistant Conductor to Wayne Marshall<br /> 
-           Venue: Opera de Lyon`,
-},
-{
-    src: Shirine,
-    alt: 'Opera de Lyon Nouvelle Production Shirine',
-    text: `<b>Shirine - a world premiere by Thierry Escaich</b> May 2022 
-           <br />Role: Assistant Conductor to Franc Ollu<br /> 
-           Venue: Opera de Lyon`,
-},
+        {
+            src: laFemme,
+            alt: 'laFemme',
+            text: `<b>Die Frau ohne Schatten</b> October 2023 
+                  <br />Role: Assistant Conductor to Daniele Rustioni<br /> 
+                  Venue: Opera de Lyon
+                  `,
+        },
+        {
+            src: mikesuite,
+            alt: 'mike in a nice blue suit',
+            text: `<b>Katia Kabanova</b> February 2023 
+                  <br />Role: Guest Conductor<br /> 
+                  Venue: 
+                  `,
+        },
+        {
+            src: Katya,
+            alt: 'Katya',
+            text: `<b>Katia Kabanova, Janacek</b> May 2023 
+                  <br />Role: Assistant Conductor to Elena Schwartz<br /> 
+                  Venue: Opera de Lyon`,
+        },
+        {
+            src: conductingOpera,
+            alt: 'Lambeth Orchestra',
+            text: `<b>Lambeth Orchestra 50th Anniversary Concert</b> February 2023 
+                  <br />Role: Principal Guest Conductor<br /> 
+                  Venue: Queen Elizabeth Hall<br />
+                  A celebratory programme including Strauss' Four Last Songs<br />
+                  Soloist: Nadine Benjamin`,
+        },
+        {
+            src: Candide,
+            alt: 'Bernstein Candide',
+            text: `<b>Candide, Bernstein</b> December 2022 
+                  <br />Role: Assistant Conductor to Wayne Marshall<br /> 
+                  Venue: Opera de Lyon`,
+        },
+        {
+            src: Shirine,
+            alt: 'Opera de Lyon Nouvelle Production Shirine',
+            text: `<b>Shirine - a world premiere by Thierry Escaich</b> May 2022 
+                  <br />Role: Assistant Conductor to Franc Ollu<br /> 
+                  Venue: Opera de Lyon`,
+        },
     ];
 
     return (
