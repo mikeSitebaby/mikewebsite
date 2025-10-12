24537cbc416335959e2e5db56bf5fc2bd67c43db
Michael Cobb <michaeltccobb@gmail.com>
2025-10-12 13:05:47 +0200

    Clean update to My Work without large images

--- src/Pages/MyWork.js
+++ src/Pages/MyWork.js
@@ -14,6 +14,7 @@ import VixenLongborough from '../Images/VixenLongborough.jpg'
 
 
 
+
 export default function MyWork() {
     const [visibleTexts, setVisibleTexts] = useState({});
     const [isClickable, setIsClickable] = useState(true);
@@ -44,6 +45,7 @@ export default function MyWork() {
 
     const works = [
 
+        
         {
             src: LambethCopland,
             alt: 'Conductor Michael Cobb performing with Lambeth Orchestra in London. Photographer: Betty Laura Zapata',
